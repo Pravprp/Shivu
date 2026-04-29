15b494c007f2650a678a1fa0b0df47bc1cfeff8d
pravprp <swayamprakash85649@gmail.com>
2026-04-29 13:40:44 +0530

    Add 'Darling' and 'My Love' to user 7556891342

--- girls.py
+++ girls.py
@@ -2,9 +2,8 @@
 
 # Dictionary mapping Telegram User IDs to a list of callable names
 ALLOWED_GIRLS = {
-    6838323964: ["dora", "dore", "dorae"],
     8211677044: ["Mishti", "Amar Shona", "Amar Rosogolla","Mere Dole Shole", "Aag Lagadi Aag Lagadi Aag Lagadi Aag Lagadi Misty"],
-    7556891342: ["Devil", "Jaanu", "Mera Pyaar"],
+    7556891342: ["Devil", "Jaanu", "Mera Pyaar", "Darling", "My Love"],
     8453491983: ["KKK", "Sundari", "Meri Dhadkan"],
     6109395189: ["Amar Gublu", "Amar Luchi", "Amar Jaan"],
     8404203699: ["Chinnu", "Bangara", "Kanda", "Gundamma"]
