685da046f05f460f8fb3a25b3e884c782ebdd108
pravprp <swayamprakash85649@gmail.com>
2026-04-29 11:21:33 +0530

    Create girls.py

---
+++ girls.py
@@ -0,0 +1,7 @@
+# girls.py
+
+# Dictionary mapping Telegram User IDs to a list of callable names
+ALLOWED_GIRLS = {
+    6838323964: ["Jaanu", "Mera Pyaar", "Dora"],
+    6838323954: ["Toto", "Sir", "Mir"]
+}
