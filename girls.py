87fa17050af766d381d2d8e3ba6422bb570c9b70
pravprp <swayamprakash85649@gmail.com>
2026-04-29 13:17:29 +0530

    Update ALLOWED_GIRLS dictionary with new entries

--- girls.py
+++ girls.py
@@ -2,6 +2,9 @@
 
 # Dictionary mapping Telegram User IDs to a list of callable names
 ALLOWED_GIRLS = {
-    6838323964: ["Jaanu", "Mera Pyaar", "Dora"],
-    6838323954: ["Toto", "Sir", "Mir"]
+    6838323964: ["dora", "dore", "dorae"],
+    8211677044: ["Mishti", "Amar Shona", "Amar Rosogolla","Mere Dole Shole", "Aag Lagadi Aag Lagadi Aag Lagadi Aag Lagadi Misty"]
+    7556891342: ["Devil", "Jaanu", "Mera Pyaar"],
+    8453491983: ["KKK", "Sundari", "Meri Dhadkan"]
+    6109395189: ["Amar Gublu", "Amar Luchi", "Amar Jaan"]
 }
