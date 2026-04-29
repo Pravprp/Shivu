648b8906e7552e192b441a0a5ea25d0952298899
pravprp <swayamprakash85649@gmail.com>
2026-04-29 14:42:31 +0530

    Add 'Kanchana' and 'Kanchi meri jaan' to list

--- girls.py
+++ girls.py
@@ -6,7 +6,7 @@ ALLOWED_GIRLS = {
     7556891342: ["Devil", "Jaanu", "Mera Pyaar", "Darling", "My Love", "Baabu Sunna"],
     8453491983: ["KKK", "Sundari", "Meri Dhadkan"],
     6109395189: ["Amar Gublu", "Amar Luchi", "Amar Jaan"],
-    8404203699: ["Chinnu", "Bangara", "Kanda", "Gundamma"],
+    8404203699: ["Kanchana","Kanchi meri jaan", "Chinnu", "Bangara", "Kanda", "Gundamma"],
     8547157813: ["Kittu", "Kittu Meri Jaan", "Jaanu", "My Love"],
     1704559662: ["Ash", "Darling", "Sweetheart", "My Love", "Sunna Meri Sapnon ki Raani"],
     8089253595: ["Nayra", "Cutie", "Suno Sundari", "My Love"]
