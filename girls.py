fa3b345dcbc4125d91ffc5572f9eb835201b686e
pravprp <swayamprakash85649@gmail.com>
2026-04-29 16:15:18 +0530

    Update ALLOWED_GIRLS with additional names
    Expanded the list of callable names for existing Telegram User IDs and added new names.

--- girls.py
+++ girls.py
@@ -2,12 +2,12 @@
 
 # Dictionary mapping Telegram User IDs to a list of callable names
 ALLOWED_GIRLS = {
-    8211677044: ["Mishti", "Amar Shona", "Amar Rosogolla","Mere Dole Shole", "Aag Lagadi Aag Lagadi Aag Lagadi Aag Lagadi Misty"],
-    7556891342: ["Devil", "Jaanu", "Mera Pyaar", "Darling", "My Love", "Baabu Sunna"],
-    8453491983: ["KKK", "Sundari", "Meri Dhadkan"],
+    8211677044: ["Mishti", "Amar Shona", "Amar Rosogolla","Mere Dole Shole", "Aag Lagadi Aag Lagadi Aag Lagadi Aag Lagadi Misty", "Amar Tuntuni", "Amar Moyna", "Amar Pakhi","Amar Bhalobasha", "Amar Rosogolla","Amar Rosogolla","Amar Rosogolla","Amar Rosogolla"],
+    7556891342: ["Devil", "Jaanu", "Mera Pyaar", "Darling", "My Love", "Baabu Sunna", "Baabu"],
+    8453491983: ["KKK", "Sundari", "Meri Dhadkan","Meri Pari"],
     6109395189: ["Amar Gublu", "Amar Luchi", "Amar Jaan"],
     8404203699: ["Kanchana","Kanchi meri jaan", "Chinnu", "Bangara", "Kanda", "Gundamma"],
-    8547157813: ["Kittu", "Kittu Meri Jaan", "Jaanu", "My Love"],
+    8547157813: ["Kittu", "Kittu Meri Jaan", "Jaanu", "My Love", "Meri Pyari Gudiya"],
     1704559662: ["Ash", "Darling", "Sweetheart", "My Love", "Sunna Meri Sapnon ki Raani"],
     8089253595: ["Nayra", "Cutie", "Suno Sundari", "My Love"]
 }
